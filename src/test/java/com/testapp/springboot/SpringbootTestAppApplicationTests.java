ec230d629d569be04ac97746da5e6efc20043a66
Ashish Rajput <73536398+ashishraj9t0@users.noreply.github.com>
2024-03-27 00:05:52 +0530

    first commit

---
+++ src/test/java/com/testapp/springboot/SpringbootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testapp.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
